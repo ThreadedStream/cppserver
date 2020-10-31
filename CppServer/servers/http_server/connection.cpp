be62fe176184e4078a5a237c683b368063f0eacf
ThreadedStream <frankflipper3@gmail.com>
2020-10-31 19:04:01 +0300

    trying to figure out a way to setup db connection

--- CppServer/servers/http_server/connection.cpp
+++ CppServer/servers/http_server/connection.cpp
@@ -1,8 +1,6 @@
 #include "connection.hpp"
 
 
-
-
 void connection::load_file(const std::string& filename)
 {
 	std::ifstream stream(filename, std::ios::in);
@@ -22,12 +20,29 @@ void connection::load_file(const std::string& filename)
 
 void connection::initDBConnection(err_code& err)
 {
-	if (!connInstance->init_conn())
+	/*if (!connInstance->init_conn())
 	{
 		err.cause = "Failed to initialize connection to a database\n";
-	}
+	}*/
 }
 	
+
+void connection::write_headers_to_file(const std::string& str, err_code& err)
+{
+	if (!dblog.is_open())
+	{
+		err.cause = "Failed to open dblog file\n";
+		return;
+	}
+	else {
+		dblog.write(str.c_str(), str.length());
+		if (dblog.fail())
+		{
+			err.cause = "Failed to write to a file\n";
+		}
+	}
+}
+
 //Main(so-called "driver") method
 void connection::start_processing()
 {
@@ -43,17 +58,25 @@ void connection::start_processing()
 		Logger::log(SEVERITY::ERR, err.cause);
 	}
 	
-	
 	auto self(shared_from_this());
 	//load_file(FILENAME);
 	socket_.async_read_some(buffer(buffer_), [&, self](const errc& err, size_t length) {
 		if (!err)
 		{
+			
 			Logger::log(SEVERITY::DEBUG, "Message: " + std::string(buffer_.c_array()));
+			err_code err_c;
+			write_headers_to_file(buffer_.c_array(), err_c);
+			if (err_c.cause != "")
+			{
+				Logger::log(SEVERITY::DEBUG, err_c.cause);
+			}
+			else {
+				Logger::log(SEVERITY::DEBUG, "Headers are written to a file\n");
+			}
 			request req(buffer_.c_array());
 			std::string str = resp.buildResponse(req);
-
-
+			
 			for (auto& ch : str)
 			{
 				if (isascii(ch))
--- CppServer/servers/http_server/connection.hpp
+++ CppServer/servers/http_server/connection.hpp
@@ -8,7 +8,7 @@
 #define FILENAME_DEMO "F:/demo.cpp"
 #define FILENAME "index.html"
 #define TEMPLATE_DIR "F:/cppserver/CppServer/templates"
-
+#define DBLOGFILE "F:/cppserver/CppServer/dblog.txt"
 
 class connection : 
 	public boost::enable_shared_from_this<connection>,
@@ -17,10 +17,11 @@ class connection :
 public:
 	explicit connection(asio_ctx& context) :
 		//strand_(make_strand(context)),
-		connInstance(dbconn::getInstance()),
+		//connInstance(dbconn::getInstance()),
 		socket_(context),
 		//Note that trailing slash is required for proper functioning
-		resp(TEMPLATE_DIR){};
+		resp(TEMPLATE_DIR),
+		dblog{ DBLOGFILE, std::ofstream::out | std::ofstream::trunc} {};
 	
 	inline sock_t & socket() { return socket_; }
 
@@ -32,6 +33,7 @@ private:
 	void read_completion_handler(const	errc & err, size_t bytes_transferred);
 	void load_file(const std::string& filename);
 	void initDBConnection(err_code& err);
+	void write_headers_to_file(const std::string& str,err_code& err);
 
 private:
 	//fields
@@ -39,7 +41,8 @@ private:
 	//strnd strand_;
 	std::string response_string;
 	response resp;
-	boost::shared_ptr<dbconn> connInstance;
+	//boost::shared_ptr<dbconn> connInstance;
 
 	boost::array<char,8192> buffer_;
+	std::ofstream dblog;
 };
