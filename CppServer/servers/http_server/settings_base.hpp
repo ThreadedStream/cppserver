0bec3207c1bf5e9058ed63ffc9545481e689b8fc
Mr. Salvado <frankflipper3@gmail.com>
2020-11-07 12:49:52 +0300

    base dir is automatically initialized now

--- CppServer/servers/http_server/settings_base.hpp
+++ CppServer/servers/http_server/settings_base.hpp
@@ -1,15 +1,15 @@
 #pragma once
 #include "../../core/non_copyable.hpp"
-#include <string>
+#include "../../core/typedefs.hpp"
 
 //This is a base class for project settings
 class settings_base : private non_copyable{
 
 public:
-	settings_base(const std::string& basedir,
-				  const std::string& templatedir):
-				 BASE_DIR_(basedir), TEMPLATE_DIR_(basedir + "/" + templatedir){}
-
+	settings_base() {
+		std::string path = _replace_all(__FILE__, "\\", "/");
+		BASE_DIR_ = _dirname(_dirname(_dirname(_dirname(path))));
+	}
 
 	virtual ~settings_base() {}
 protected:
@@ -17,4 +17,41 @@ protected:
 	std::string TEMPLATE_DIR_ = "";
 	std::string REQ_FILE_ = "";
 	std::string STATIC_DIR_ = "";
+
+private:
+
+	/*
+		@Replaces all occurrences of "from" to "to" in given string
+	*/
+	std::string _replace_all(std::string str, const std::string& from, const std::string& to) {
+		size_t start_pos = 0;
+		while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+			str.replace(start_pos, from.length(), to);
+			start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+		}
+		return str;
+	}
+
+	/*
+		@Returns absolute path to dirname of current file.
+		Note that file path must be absolute for proper functioning.
+		It is also compulsory to alter path style from \\ to /, since __FILE__ returns 
+		path in former format.
+	*/
+	
+
+	inline std::string _dirname(const std::string& file_path)
+	{
+		/*
+		Check whether a path string has a trailing slash and,based on previous check,
+		assign a corresponding value to starting_offset
+		*/
+		ui16 i{ 0 };
+		ui16 starting_offset = (file_path[file_path.length() - 1] == '/') ? file_path.length() - 2
+																		: file_path.length() - 1;
+		auto found = file_path.find_last_of("/");
+
+
+		return file_path.substr(0, found);
+	}
 };
