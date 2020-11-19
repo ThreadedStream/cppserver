6dfa4df1d803bdc037ff3d9d5108a4907438c4bd
Mr. Salvado <frankflipper3@gmail.com>
2020-11-19 22:08:51 +0300

    problems with transferring streambuf's contents to string were resolved

--- CppServer/client/client.hpp
+++ CppServer/client/client.hpp
@@ -5,7 +5,8 @@
 struct sync_client {
 	sock_ptr_t socket_;
 	std::string reqbuf;
-
+	streambuf respbuf;
+	std::string respstr;
 	void initiate(err_code& err) {
 		if (socket_->is_open()) {
 			Logger::log(SEVERITY::DEBUG, "Attempting to write some data\n");
@@ -38,24 +39,55 @@ private:
 	}
 	void sync_read(err_code& err) {
 		errc err_read;
-		
-		streambuf respbuf;
 		try {
-			read_until(*socket_.get(), respbuf, "\r\n\r\n");
+			read(*socket_.get(), respbuf);
 		}
 		catch (const boost::system::system_error& err){
 			std::cout << err.what();
 		}
-		std::istream response_stream(&respbuf);
-		std::string http_version;
-		response_stream >> http_version;
-		unsigned int status_code;
-		response_stream >> status_code;
-		if (status_code != 200) {
-			err.cause = "Operation has failed with the following status code: " + status_code;
-			return;
-		}
-		Logger::log(SEVERITY::DEBUG, "Operation has finished successfully\n");
+		auto bufs = respbuf.data();
+		respstr = std::string(buffers_begin(bufs), buffers_begin(bufs) + respbuf.size());
 		std::cout << &respbuf;
+	}
+};
+
+struct async_client {
+	sock_ptr_t socket_;
+	std::string reqbuf;
+	streambuf respbuf;
+	void initiate(err_code& err) {
+		if (socket_->is_open()) {
+			return async_write(err);
+		}
+		else {
+			err.cause = "Socket is closed\n";
+		}
+	}
+
+	void operator()(err_code& err) {
+		return initiate(err);
+	}
+private:
+	void async_write(err_code& err) {
+		socket_->async_write_some(buffer(reqbuf, reqbuf.length()), [&](const errc& err_write, size_t written){
+			if (!err_write) {
+				async_read(err);
+			}
+			else {
+				err.cause = err_write.message();
+			}
+		});
+	}
+
+	void async_read(err_code& err) {
+		async_read_until(*socket_.get(), respbuf, "\r\n\r\n", [&](const errc& err_read, size_t written) {
+			if (!err_read) {
+				Logger::log(SEVERITY::DEBUG, "Reading operation was successfully carried out\n");
+				std::cout << &respbuf;
+			}
+			else {
+				err.cause = err_read.message();
+			}
+		});
 	}
 };
